96538f767f369a27fca3c1df027eb208b3e2e2ae
Callie Giles <anchoredinthecrown@gmail.com>
2023-11-01 17:53:03 -0400

    add breadcrumb

--- src/screens/Study.js
+++ src/screens/Study.js
@@ -52,6 +52,21 @@ function Study() {
   if (cards.length <= 2) {
     return (
       <div>
+        <nav aria-label="breadcrumb">
+          <ol className="breadcrumb">
+            <li className="breadcrumb-item">
+              <Link to="/">Home</Link>
+            </li>
+            {deck && (
+              <li className="breadcrumb-item">
+                <Link to={`/decks/${deck.id}`}>{deck.name}</Link>
+              </li>
+            )}
+            <li className="breadcrumb-item active" aria-current="page">
+              Study
+            </li>
+          </ol>
+        </nav>
         <h2>{`Study: ${deck ? deck.name : 'Deck'}`}</h2>
         <h4>Not enough cards in the deck. Add more cards to study.</h4>
         <Link to={`/decks/${deckId}/cards/new`} className="btn btn-primary">
@@ -63,6 +78,21 @@ function Study() {
 
   return (
     <div>
+      <nav aria-label="breadcrumb">
+        <ol className="breadcrumb">
+          <li className="breadcrumb-item">
+            <Link to="/">Home</Link>
+          </li>
+          {deck && (
+            <li className="breadcrumb-item">
+              <Link to={`/decks/${deck.id}`}>{deck.name}</Link>
+            </li>
+          )}
+          <li className="breadcrumb-item active" aria-current="page">
+            Study
+          </li>
+        </ol>
+      </nav>
       <h2>{`Study: ${deck ? deck.name : 'Deck'}`}</h2>
       <h4>Card {currentCardIndex + 1} of {cards.length}</h4>
       <div className="card">
