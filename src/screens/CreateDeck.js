d51b77f33f7cfe08a8a460349d223db3de6637a7
Callie Giles <anchoredinthecrown@gmail.com>
2023-11-01 18:11:17 -0400

    update CreateDeck

--- src/screens/CreateDeck.js
+++ src/screens/CreateDeck.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useHistory } from "react-router-dom";
+import { useHistory, Link } from "react-router-dom";
 import { createDeck } from "../utils/api";
 
 function CreateDeck() {
@@ -22,21 +22,32 @@ function CreateDeck() {
 
   return (
     <div>
+      <nav aria-label="breadcrumb">
+        <ol className="breadcrumb">
+          <li className="breadcrumb-item">
+            <Link to="/">Home</Link>
+          </li>
+          <li className="breadcrumb-item active" aria-current="page">
+            Create Deck
+          </li>
+        </ol>
+      </nav>
       <h2>Create Deck</h2>
       <form onSubmit={handleSubmit}>
         <div>
           <label>Name:</label>
-          <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+          <input type="text" placeholder="Deck Name" value={name} onChange={(e) => setName(e.target.value)} />
         </div>
         <div>
           <label>Description:</label>
           <textarea
+            placeholder="Brief description of the deck"
             value={description}
             onChange={(e) => setDescription(e.target.value)}
           />
         </div>
-        <button type="submit">Submit</button>
-        <button type="button" onClick={handleCancel}>Cancel</button>
+        <button className="btn btn-primary" type="submit">Submit</button>
+        <button className="btn btn-secondary" type="button" onClick={handleCancel}>Cancel</button>
       </form>
     </div>
   );
