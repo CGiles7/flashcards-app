1535ab018589014b7555ea701b664d1cb34c8287
Callie Giles <anchoredinthecrown@gmail.com>
2023-11-01 17:32:07 -0400

    add study functionality

--- src/screens/Study.js
+++ src/screens/Study.js
@@ -1,14 +1,16 @@
-import React, { useState, useEffect } from "react";
-import { Link, useParams } from "react-router-dom";
-import { readDeck } from "../utils/api";
-import CardsList from "./CardsList";
+import React, { useState, useEffect } from 'react';
+import { Link, useParams, useHistory } from 'react-router-dom';
+import { readDeck } from '../utils/api';
+import CardsList from './CardsList';
 
 function Study() {
   const { deckId } = useParams();
+  const history = useHistory();
   const [deck, setDeck] = useState(null);
   const [cards, setCards] = useState([]);
+  const [currentCardIndex, setCurrentCardIndex] = useState(0);
+  const [showBack, setShowBack] = useState(false);
 
-  // Load the deck and its cards
   useEffect(() => {
     async function loadDeck() {
       try {
@@ -23,30 +25,64 @@ function Study() {
     loadDeck();
   }, [deckId]);
 
+  const nextCard = () => {
+    // Move to the next card or back to the first card if at the end
+    setCurrentCardIndex((prevIndex) =>
+      prevIndex === cards.length - 1 ? 0 : prevIndex + 1
+    );
+    setShowBack(false); // Show the front side by default on the next card
+
+    // Check if all cards have been shown
+    if (currentCardIndex === cards.length - 1) {
+      const confirmRestart = window.confirm('Congratulations! You have completed the deck. Do you want to restart the deck?');
+      if (confirmRestart) {
+        setCurrentCardIndex(0);
+        setShowBack(false);
+      } else {
+        history.push('/');
+      }
+    }
+  };
+
+  const flipCard = () => {
+    // Toggle the state to flip the card
+    setShowBack((prevShowBack) => !prevShowBack);
+  };
+
+  if (cards.length <= 2) {
+    return (
+      <div>
+        <h2>{`Study: ${deck ? deck.name : 'Deck'}`}</h2>
+        <h4>Not enough cards in the deck. Add more cards to study.</h4>
+        <Link to={`/decks/${deckId}/cards/new`} className="btn btn-primary">
+          Add Cards
+        </Link>
+      </div>
+    );
+  }
+
   return (
     <div>
-      <nav aria-label="breadcrumb">
-        <ol className="breadcrumb">
-          <li className="breadcrumb-item">
-            <Link to="/">Home</Link>
-          </li>
-          <li className="breadcrumb-item">
-            {deck ? (
-              <Link to={`/decks/${deck.id}/study`}>{deck.name}</Link>
-            ) : (
-              'Deck'
-            )}
-          </li>
-          <li className="breadcrumb-item active" aria-current="page">
-            Study
-          </li>
-        </ol>
-      </nav>
-
-      {deck ? <h2>{`Study: ${deck.name}`}</h2> : null}
-
-      <CardsList decks={deck ? [deck] : []} cards={cards} />
-      {/* Additional study components */}
+      <h2>{`Study: ${deck ? deck.name : 'Deck'}`}</h2>
+      <h4>Card {currentCardIndex + 1} of {cards.length}</h4>
+      <div className="card">
+        <div className="card-body">
+          <h5 className="card-title">
+            {showBack ? 'Back' : 'Front'}
+          </h5>
+          <p className="card-text">
+            {showBack ? cards[currentCardIndex].back : cards[currentCardIndex].front}
+          </p>
+          <button className="btn btn-secondary" onClick={flipCard}>
+            Flip
+          </button>
+          {showBack && (
+            <button className="btn btn-primary" onClick={nextCard}>
+              Next
+            </button>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
